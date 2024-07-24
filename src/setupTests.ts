6b575c67c54821e37382ac72de328f13bbbf6e26
Yiwei Wu <yiweiwu0128@gmail.com>
2024-07-24 17:20:57 +1000

    change test library from jest to vitest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,8 @@
 // src/setupTests.ts
-import '@testing-library/jest-dom';
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+afterEach(() => {
+  cleanup();
+});
